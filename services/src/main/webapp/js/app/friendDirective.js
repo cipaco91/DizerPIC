29565a074d7f1a2e9119bd764eb5a84421b98f1f
Ciprian Paraschivescu <ciprian.paraschivescu@gmail.com>
2015-04-07 17:50:54 +0300

    create friend directive

--- services/src/main/webapp/js/app/friendDirective.js
+++ services/src/main/webapp/js/app/friendDirective.js
@@ -1,11 +1,11 @@
-app.directive('appShowresults', function () {
+app.directive('appFriendDirective', function () {
     return {
         restrict: 'E',
         replace: true,
         templateUrl: "partials/common/friendDirective.html",
         controller: function ($scope) {
-            $scope.choiceNbResults = [ 10, 50, 100, 1000, 10000 ];
-            $scope.nbResults = 50
+            //$scope.choiceNbResults = [ 10, 50, 100, 1000, 10000 ];
+            //$scope.nbResults = 50
         }
     }
 });
