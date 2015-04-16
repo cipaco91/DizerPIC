b5ca5d9c5530d680290c4df2513b6f4c426f8614
Ciprian Paraschivescu <ciprian.paraschivescu@gmail.com>
2015-04-16 17:08:51 +0300

    add wizard

--- services/src/main/webapp/js/app/controllers/settingsController.js
+++ services/src/main/webapp/js/app/controllers/settingsController.js
@@ -1,10 +1,49 @@
-function SettingsCtrl($scope, $location) {
+function SettingsCtrl($scope, $location,$window) {
 
     $scope.step = 1;
+    $scope.step1=true;
+    $scope.step2=false;
+    $scope.step3=false;
+
+    $scope.first='active';
+    $scope.social='inactive';
+    $scope.personal='inactive';
 
     $scope.setStep = function(step){
-        $scope.step = step+1;
+        if(step == 1){
+            $scope.step1=false;
+            $scope.step2=true;
+            $scope.step3=false;
+            $scope.first='active';
+            $scope.social='active';
+            $scope.personal='inactive';
+        }else if(step == 2){
+            $scope.step1=false;
+            $scope.step2=false;
+            $scope.step3=true;
+            $scope.first='active';
+            $scope.social='active';
+            $scope.personal='active';
+        }else{
+            $window.location.href="http://localhost:8080/social/#/profile"
+        }
     }
 
-
+    $scope.setStepBack = function(step){
+         if(step == 2){
+            $scope.step1=true;
+            $scope.step2=false;
+            $scope.step3=false;
+             $scope.first='active';
+             $scope.social='inactive';
+             $scope.personal='inactive';
+        }else{
+             $scope.step1=false;
+             $scope.step2=true;
+             $scope.step3=false;
+             $scope.first='active';
+             $scope.social='active';
+             $scope.personal='inactive';
+        }
+    }
 }
