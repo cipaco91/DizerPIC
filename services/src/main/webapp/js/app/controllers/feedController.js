620d3b448e5be5fdad8aab4af128ad2a97cf42cf
Ciprian Paraschivescu <ciprian.paraschivescu@gmail.com>
2015-05-28 17:36:12 +0300

    add google plus

--- services/src/main/webapp/js/app/controllers/feedController.js
+++ services/src/main/webapp/js/app/controllers/feedController.js
@@ -1,11 +1,13 @@
-function FeedCtrl($scope, $location,FeedService,ProfileService,MenuService,$controller,$sce) {
+function FeedCtrl($scope, $location, FeedService, ProfileService, MenuService, $controller, $sce) {
 
     angular.extend(this, $controller('ProfileCtrl', {$scope: $scope}));
 
-    $scope.feedTwitter={};
-    $scope.feedLinkedin={};
-    $scope.feedFacebook={};
+    $scope.feedTwitter = {};
+    $scope.feedLinkedin = {};
+    $scope.feedFacebook = {};
     $scope.date = new Date();
+    $scope.postVisible = false;
+    $scope.postText = "";
 
     //FeedService.findFeedTwitter().
     //    success(function (users) {
@@ -28,14 +30,57 @@ function FeedCtrl($scope, $location,FeedService,ProfileService,MenuService,$cont
         FeedService.addLike(feedId);
     };
 
-    $scope.addComment = function (feedId) {
-        console.log(feedId);
-        FeedService.addComment(feedId,':)');
+    $scope.changePost = function () {
+        if ($scope.postText != undefined && $scope.postText != "") {
+            $scope.postVisible = true;
+        } else {
+            $scope.postVisible = false;
+        }
     };
 
-    $scope.trustSrc = function(src) {
+    $scope.addComment = function (feedId) {
+        console.log(feedId);
+        FeedService.addComment(feedId, ':)');
+    };
+
+    $scope.trustSrc = function (src) {
         return $sce.trustAsResourceUrl(src);
-    }
+    };
+    $scope.clickFacebook = function (src) {
+        if ($scope.classFacebook == "classFacebook") {
+            $scope.classFacebook = "";
+        }
+        $scope.classFacebook = "classFacebook";
+    };
+
+    $scope.clickTwitter = function (src) {
+        if ($scope.classTwitter == "classTwitter") {
+            $scope.classTwitter = "";
+        }
+        $scope.classTwitter = "classTwitter";
+    };
+
+    $scope.clickLinkedin = function (src) {
+        if ($scope.classLinkedin == "classLinkedin") {
+            $scope.classLinkedin = "";
+        }
+        $scope.classLinkedin = "classLinkedin";
+    };
+
+    $scope.clickGoogle = function (src) {
+        if ($scope.classGoogle == "classGoogle") {
+            $scope.classGoogle = "";
+        }
+        $scope.classGoogle = "classGoogle";
+    };
+
+    $scope.postAction = function (src) {
+        $scope.classGoogle = "";
+        $scope.classLinkedin = "";
+        $scope.classTwitter = "";
+        $scope.classFacebook = "";
+        $scope.postText="";
+    };
 
     //FeedService.findFeedLinkedin().
     //    success(function (users) {
@@ -44,7 +89,6 @@ function FeedCtrl($scope, $location,FeedService,ProfileService,MenuService,$cont
     //    .error(function (resp) {
     //        console.log("Error with FriendsService.findFriendsTwitter" + resp);
     //    });
-
 
 
 }
